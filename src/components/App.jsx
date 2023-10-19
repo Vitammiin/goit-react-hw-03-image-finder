034d72ada05bfb8d0c59f06a9db4b6686b6f5aac
Christian <120150697+Vitammiin@users.noreply.github.com>
2023-10-18 21:53:37 -0400

    index.html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 // import Button from './Button/Button'
 import { fetchPosts } from 'servis/API';
-// import ImageGallery from './ImageGallery/ImageGallery';
+import ImageGallery from './ImageGallery/ImageGallery';
 // import Loader from './Loader/Loader'
 // import Modal from './Modal/Modal'
 import Searchbar from './Searchbar/Searchbar';
@@ -33,6 +33,7 @@ export class App extends Component {
       <div>
         <div>
           <Searchbar />
+          <ImageGallery images={this.state.images} />
         </div>
       </div>
     );
